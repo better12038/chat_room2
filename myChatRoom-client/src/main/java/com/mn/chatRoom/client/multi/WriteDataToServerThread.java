51298dab1fa101c7b0a470a3a90fd1550e4fac64
better12038 <1873745795@qq.com>
2019-07-19 19:52:29 +0800

    1.0.0

---
+++ myChatRoom-client/src/main/java/com/mn/chatRoom/client/multi/WriteDataToServerThread.java
@@ -0,0 +1,44 @@
+package com.mn.chatRoom.client.multi;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class WriteDataToServerThread extends Thread{
+    private final Socket client;
+
+    public WriteDataToServerThread(Socket client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //获取客户端的输出流
+            OutputStream clientOutput = client.getOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(clientOutput);
+
+            Scanner scanner = new Scanner(System.in);
+            while (true){
+                System.out.println("请输入消息：");
+                String message = scanner.nextLine();
+
+                //往服务器发送数据
+                writer.write(message+"\n");
+                writer.flush();
+
+                //客户端要关闭
+                if(message.equals("bye")){
+                    client.close();
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+    }
+}
+
---
+++ myChatRoom-server/src/main/java/com/mn/chatRoom/server/multi/ServerMain.java
@@ -0,0 +1,35 @@
+package com.mn.chatRoom.server.multi;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 9999;
+        if(args.length > 0){
+            try{
+                port = Integer.parseInt(args[0]);
+            }catch(NumberFormatException e){
+                System.out.println("端口参数不对，采用默认端口" + port);
+            }
+        }
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(10);
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            System.out.println("等待客户端连接中...");
+
+            while(true){
+                Socket client = serverSocket.accept();
+                executorService.submit(new ExecuteClient(client));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
